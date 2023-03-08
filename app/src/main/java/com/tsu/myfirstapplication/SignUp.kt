7abdc9ecd779d41dca0a5dfc1bea0558c958aad6
Sugoma <61140354+BorisYouWillMiss@users.noreply.github.com>
2023-03-08 18:23:18 +0700

    Backbutton from signup

--- app/src/main/java/com/tsu/myfirstapplication/SignUp.kt
+++ app/src/main/java/com/tsu/myfirstapplication/SignUp.kt
@@ -1,11 +1,27 @@
 package com.tsu.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.tsu.myfirstapplication.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private var _binding: ActivitySignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        _binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        findViewById<Button>(R.id.buttonBack).setOnClickListener{
+            Log.d("Tagtag", "Button pressed")
+            val intent = Intent(this@SignUp, ViewPagerActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
